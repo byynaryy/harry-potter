3680f35579af3d3910e69c30f5e5387ae28ca0f8
Jeff <Home@Jeffs-MacBook-Pro.local>
2017-07-16 19:17:08 +0200

    Harry Potter

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # harry-potter
 This harry-potter is a library with all the seven series of Harry Potter novels. If a client buys all the seven series she/he gets one free or if a client buys less that seven different copies of the series, she/he gets a discount, depending on the number of series:
 
-6 books - 12% off
-5 books - 11% off
-4 books - 9% off
+6 books - 16% off
+5 books - 13% off
+4 books - 10% off
 3 books - 7% off
 2 books - 4% off
 
--- harry-potter.js
+++ harry-potter.js
@@ -8,3 +8,43 @@ var harryPotter = [
     {title:"Harry Potter and the Deathly Hallows", cost:15},
 ]
 
+
+
+var totalCost = function(...titles) {
+    var price = 0;
+    var otherPrice = 0;
+    var k = 0;
+    var expensiveNovel = obj.sort(function(a, b){return b.cost - a.cost});
+    if(titles.length == [...new Set(titles)].length && titles.length == 7) {
+        price = obj.reduce((a, b) => ({cost: a.cost + b.cost}))
+        return price.cost - expensiveNovel[0].cost;
+    }
+                
+    for(var i = 0; i < obj.length; i++) {
+        var unique = true;
+        for(var j = 0; j < titles.length; j++) {
+            if((obj[i].title == titles[j]) && unique) {
+                price += obj[i].cost;
+                unique = false;
+                k += 1;
+            }else if ((obj[i].title == titles[j]) && unique === false ) {
+                otherPrice += obj[i].cost;
+            }
+        }
+    }
+    return discount(k, price, otherPrice);
+}
+
+
+var discount = function(nbrOfUniqueNovels, price, otherPrice) {
+    if(nbrOfUniqueNovels == 2) {
+        return price * 0.96 + otherPrice;
+    }else if(nbrOfUniqueNovels == 3) {
+        return price * 0.93 + otherPrice
+    }else if(nbrOfUniqueNovels == 4) {
+        return price * 0.90 + otherPrice;
+    }else if(nbrOfUniqueNovels == 5) {
+        return price * 0.87 + otherPrice;
+    }else {
+        return price * 0.84 + otherPrice;                                                            }
+}
