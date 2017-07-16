0f798caf4b8d2e2c4e300122baade3c119faf5cf
Jeff <Home@Jeffs-MacBook-Pro.local>
2017-07-16 16:34:14 +0200

    An array of objects

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # harry-potter
+This harry-potter is a library with all the seven series of Harry Potter novels. If a client buys all the seven series she/he gets one free or if a client buys less that seven different copies of the series, she/he gets a discount, depending on the number of series:
+
+6 books - 12% off
+5 books - 11% off
+4 books - 9% off
+3 books - 7% off
+2 books - 4% off
+
+Note: No discount on a second or several copies of the same series. 
---
+++ harry-potter.js
@@ -0,0 +1,10 @@
+var harryPotter = [ 
+    {title:"Harry Potter and the Philosopher's Stone", cost:15},
+    {title:"Harry Potter and the Chamberof Secrets", cost:15},
+    {title:"Harry Potter and the Prisoner of Azkaban", cost:15},
+    {title:"Harry Potter and the Goblet of Fire", cost:15},
+    {title:"Harry Potter and the Order of the Phoenix", cost:15},
+    {title:"Harry Potter and the Half-Blood Prince", cost:15},
+    {title:"Harry Potter and the Deathly Hallows", cost:15},
+]
+
